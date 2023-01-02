c63f6dc1d87020ec4bbc58b68bb73253aa365d78
Swetabh Changkakoti <50801120+swetabhch@users.noreply.github.com>
2023-01-02 12:15:42 +0530

    finished a draft of the parser, need more testing

--- src/cnf.rs
+++ src/cnf.rs
@@ -37,6 +37,8 @@ pub mod cnf {
 }
 
 pub mod parser {
+    use std::collections::HashSet;
+
     use super::cnf::*;
     use thiserror::Error;
 
@@ -50,16 +52,93 @@ pub mod parser {
         MalformedClauseEnd,
         #[error("literal is not in the form of a signed int")]
         MalformedLiteral,
-        #[error("cannot parse empty formula")]
-        EmptyFormula,
+        #[error("comments, specification, clauses sequencing incorrect")]
+        BadSequencing,
+        #[error("specfication line incorrectly formatted")]
+        BadSpecification,
+        #[error("specification does not match formula")]
+        SpecificationMismatch,
+        #[error("no specification line (eg. 'p cnf 3 4')")]
+        NoSpecification,
     }
 
     /// Converts a string describing a CNF formula in the DIMACS format
     /// into a CNFFormula representation.
     /// Assumption: a clause can only take up one line.
     /// (See 'CNF Input Format' on page 4 of http://www.satcompetition.org/2011/rules.pdf)
-    pub fn parse_dimacs_cnf(dimacs_str: &str) -> Result<CNFFormula, &str> {
-        Ok(CNFFormula { clauses: vec![] })
+    pub fn parse_dimacs_cnf(dimacs_str: &str) -> Result<CNFFormula, ParseError> {
+        let mut trimmed_strs: Vec<&str> = dimacs_str.lines().map(&str::trim).collect();
+
+        // Check that c, p, content lines are correctly sequenced and specification info
+        let mut p_idx = None;
+        let mut spec_line = "";
+        for (idx, line) in trimmed_strs.iter().enumerate() {
+            if (line.starts_with("c") || line.starts_with("p cnf")) && p_idx.is_some() {
+                return Err(ParseError::BadSequencing);
+            }
+            if line.starts_with("p cnf") {
+                p_idx = Some(idx);
+                spec_line = line;
+            }
+        }
+
+        // Parse content lines
+        let split_idx = match p_idx {
+            None => return Err(ParseError::NoSpecification),
+            Some(idx) => idx,
+        };
+        let (num_vars, num_clauses) = parse_spec_line(spec_line)?;
+        let mut clauses = Vec::new();
+        for clause_str in trimmed_strs.drain((split_idx + 1)..) {
+            clauses.push(parse_line_to_clause(clause_str)?);
+        }
+
+        // Validate obtained formula against specification in 'p cnf' line
+        let formula = CNFFormula { clauses };
+        if validate_formula(&formula, num_vars, num_clauses) {
+            Ok(formula)
+        } else {
+            Err(ParseError::SpecificationMismatch)
+        }
+    }
+
+    /// Determines whether a CNFFormula has the specified number of vars and clauses.
+    fn validate_formula(formula: &CNFFormula, num_vars: u32, num_clauses: u32) -> bool {
+        if formula.clauses.len() != num_clauses.try_into().unwrap() {
+            return false;
+        }
+        let mut distinct_vars = HashSet::new();
+        let insert_vars = |clause: &Clause| {
+            clause.literals.iter().for_each(|l| {
+                distinct_vars.insert(l.name);
+            })
+        };
+        formula.clauses.iter().for_each(insert_vars);
+        distinct_vars.len() <= num_vars.try_into().unwrap()
+    }
+
+    /// Parses a specification line in DIMACS format and returns a Result containing
+    /// a tuple with the number of vars and the number of clauses.
+    /// Assumes that the input starts with "p cnf"
+    fn parse_spec_line(spec_line: &str) -> Result<(u32, u32), ParseError> {
+        let mut spec_iter = spec_line.split_ascii_whitespace();
+        spec_iter.next();
+        spec_iter.next();
+        let num_vars = match spec_iter.next() {
+            None => return Err(ParseError::BadSpecification),
+            Some(v_str) => match v_str.parse::<u32>() {
+                Ok(n) => n,
+                Err(_) => return Err(ParseError::BadSpecification),
+            },
+        };
+        let num_clauses = match spec_iter.next() {
+            None => return Err(ParseError::BadSpecification),
+            Some(v_str) => match v_str.parse::<u32>() {
+                Ok(n) => n,
+                Err(_) => return Err(ParseError::BadSpecification),
+            },
+        };
+        Ok((num_vars, num_clauses))
     }
 
     /// Takes in a line as input. If it is in DIMACS format and correctly represents a
@@ -110,6 +189,155 @@ pub mod parser {
     mod tests {
         use super::*;
 
+        // --- parse_dimacs_cnf ---
+        #[test]
+        fn link_example() {
+            let s = "c
+          c start with comments
+          c
+          c
+          p cnf 5 3
+          1 -5 4 0
+          -1 5 3 4 0
+          -3 -4 0";
+            let c1 = Clause {
+                literals: vec![
+                    Literal::new(1, Sign::Positive),
+                    Literal::new(5, Sign::Negative),
+                    Literal::new(4, Sign::Positive),
+                ],
+            };
+            let c2 = Clause {
+                literals: vec![
+                    Literal::new(1, Sign::Negative),
+                    Literal::new(5, Sign::Positive),
+                    Literal::new(3, Sign::Positive),
+                    Literal::new(4, Sign::Positive),
+                ],
+            };
+            let c3 = Clause {
+                literals: vec![
+                    Literal::new(3, Sign::Negative),
+                    Literal::new(4, Sign::Negative),
+                ],
+            };
+            match parse_dimacs_cnf(s) {
+                Err(_) => assert!(false),
+                Ok(fmla) => assert_eq!(fmla.clauses, vec![c1, c2, c3]),
+            }
+        }
+
+        // --- validate_formula ---
+
+        // TODO: add negative tests
+
+        #[test]
+        fn validate_empty_formula() {
+            assert!(validate_formula(&CNFFormula { clauses: vec![] }, 0, 0));
+        }
+
+        #[test]
+        fn validate_formula_with_empty_clause() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![Clause { literals: vec![] }]
+                },
+                0,
+                1
+            ));
+        }
+
+        #[test]
+        fn validate_formula_one_var_one_clause() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![Clause {
+                        literals: vec![Literal::new(1, Sign::Positive)]
+                    }]
+                },
+                1,
+                1
+            ));
+        }
+
+        #[test]
+        fn validate_formula_one_var_repeated_one_clause() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![Clause {
+                        literals: vec![
+                            Literal::new(1, Sign::Positive),
+                            Literal::new(1, Sign::Negative)
+                        ]
+                    }]
+                },
+                1,
+                1
+            ));
+        }
+
+        #[test]
+        fn validate_formula_one_var_multiple_clauses() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![
+                        Clause {
+                            literals: vec![Literal::new(1, Sign::Positive)]
+                        },
+                        Clause {
+                            literals: vec![Literal::new(1, Sign::Negative)]
+                        }
+                    ]
+                },
+                1,
+                2
+            ));
+        }
+
+        #[test]
+        fn validate_formula_one_clause_multiple_vars() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![Clause {
+                        literals: vec![
+                            Literal::new(1, Sign::Positive),
+                            Literal::new(2, Sign::Negative)
+                        ]
+                    }]
+                },
+                2,
+                1
+            ));
+        }
+
+        #[test]
+        fn validate_formula_multi_clauses_multi_vars() {
+            assert!(validate_formula(
+                &CNFFormula {
+                    clauses: vec![
+                        Clause {
+                            literals: vec![
+                                Literal::new(1, Sign::Positive),
+                                Literal::new(2, Sign::Negative)
+                            ]
+                        },
+                        Clause {
+                            literals: vec![
+                                Literal::new(2, Sign::Positive),
+                                Literal::new(3, Sign::Negative)
+                            ]
+                        },
+                        Clause { literals: vec![] },
+                        Clause {
+                            literals: vec![Literal::new(1, Sign::Negative)]
+                        }
+                    ]
+                },
+                3,
+                4
+            ));
+        }
+
         // --- parse_line_to_clause ---
         #[test]
         fn parse_valid_empty_clause() {
@@ -125,6 +353,122 @@ pub mod parser {
                 parse_line_to_clause(&""),
                 Err(ParseError::MalformedClauseEnd)
             );
+        }
+
+        #[test]
+        fn parse_needs_clause_to_end_with_zero() {
+            assert_eq!(
+                parse_line_to_clause(&"1"),
+                Err(ParseError::MalformedClauseEnd)
+            );
+        }
+
+        #[test]
+        fn parse_line_to_clause_zero_present_at_non_terminal_posn() {
+            assert_eq!(
+                parse_line_to_clause(&"5 -6 -1 0 2"),
+                Err(ParseError::MalformedClauseEnd)
+            )
+        }
+
+        #[test]
+        fn parse_line_to_clause_errors_on_malformed_literal() {
+            assert_eq!(
+                parse_line_to_clause(&"5 -6 - 2 0"),
+                Err(ParseError::MalformedLiteral)
+            );
+            assert_eq!(
+                parse_line_to_clause(&"5 -6 c 2 0"),
+                Err(ParseError::MalformedLiteral)
+            );
+            assert_eq!(
+                parse_line_to_clause(&"! -6 -1 2 0"),
+                Err(ParseError::MalformedLiteral)
+            );
+        }
+
+        #[test]
+        fn parse_line_to_clause_errors_when_overflowing_var_name() {
+            assert_eq!(
+                parse_line_to_clause(&format!("1 {} 0", i32::to_string(&i32::MIN))),
+                Err(ParseError::VarNameTooLarge)
+            )
+        }
+
+        #[test]
+        fn parse_line_to_clause_trims_input() {
+            assert_eq!(
+                parse_line_to_clause(&"   0  ").unwrap(),
+                Clause { literals: vec![] }
+            );
+        }
+
+        #[test]
+        fn parse_line_to_clause_single_literal() {
+            assert_eq!(
+                parse_line_to_clause(&" -1 0"),
+                Ok(Clause {
+                    literals: vec![Literal::new(1, Sign::Negative)]
+                })
+            )
+        }
+
+        #[test]
+        fn parse_line_to_clause_multiple_literals() {
+            assert_eq!(
+                parse_line_to_clause(&"5 -6 -1 2 0"),
+                Ok(Clause {
+                    literals: vec![
+                        Literal::new(5, Sign::Positive),
+                        Literal::new(6, Sign::Negative),
+                        Literal::new(1, Sign::Negative),
+                        Literal::new(2, Sign::Positive)
+                    ]
+                })
+            )
+        }
+
+        // --- parse_spec_line ---
+        #[test]
+        fn parse_spec_line_with_no_nums() {
+            assert_eq!(parse_spec_line("p cnf"), Err(ParseError::BadSpecification));
+        }
+
+        #[test]
+        fn parse_spec_line_with_one_num() {
+            assert_eq!(
+                parse_spec_line("p cnf 5"),
+                Err(ParseError::BadSpecification)
+            );
+        }
+
+        #[test]
+        fn parse_spec_line_with_non_num() {
+            assert_eq!(
+                parse_spec_line("p cnf f"),
+                Err(ParseError::BadSpecification)
+            );
+        }
+
+        #[test]
+        fn parse_spec_line_with_one_num_one_char() {
+            assert_eq!(
+                parse_spec_line("p cnf 5 f"),
+                Err(ParseError::BadSpecification)
+            );
+        }
+
+        #[test]
+        fn parse_spec_line_with_neg_num() {
+            assert_eq!(
+                parse_spec_line("p cnf 3 -4"),
+                Err(ParseError::BadSpecification)
+            );
+        }
+
+        #[test]
+        fn parse_spec_line_with_two_non_neg_nums() {
+            assert_eq!(parse_spec_line("p cnf 3 4"), Ok((3, 4)));
         }
 
         // --- parse_num_to_literal ---
